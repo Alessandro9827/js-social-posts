2a35336b2a9f26af6bd567b6de5e8b9377181b1d
Alessandro9827 <ale.moramarco27@gmail.com>
2023-12-02 19:18:55 +0100

    crated index html scrip js and style css

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//Ricreiamo un feed social aggiungendo al layout di base fornito.
+//1)Creiamo il nostro array di oggetti che rappresentano ciascun post.
+//2)stampiamo i post del nostro feed.
+//3)tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
